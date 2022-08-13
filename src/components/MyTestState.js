0758d5204c7356a1e12ce2a276b0e91d64160ea7
Igor Kotljar <kotljar.igor@gmail.com>
2022-08-12 21:21:31 -0700

    added if condition to the state

--- src/components/MyTestState.js
+++ src/components/MyTestState.js
@@ -3,13 +3,19 @@ import React, {useState} from 'react'
 
 function MyTestState() {
 
-    const [count, setCount] = useState(0);
-
+    let [count, setCount] = useState(null);
+    let msg = "You clicked this amount of times: "
+ 
+    if (count === 11) {
+        count = null;
+        msg = "Lets try again"
+    }
+    
 
   return (
     <div style={{textAlign: "center", margin: 10, padding: 10}}>
     <Button onClick={() => setCount(count + 1)} variant='outlined'> Click to count</Button>
-      <p>You clicked {count} times</p>
+      <p>{msg} {count} </p>
     </div>
   )
 }
