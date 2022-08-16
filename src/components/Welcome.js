f4fdeb525a611543573a74aa83a59818d807baaa
Igor Kotljar <kotljar.igor@gmail.com>
2022-08-16 09:37:31 -0700

    first build

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,10 +1,12 @@
 import { Button } from "@mui/material";
 import React from "react";
+import Logo from "../styles/img/348s (2).png"
 
 function Welcome() {
   return (
     <div className="welcomeText">
-      <h1 style={{fontSize: 45}}>"Train with both heart and soul without worrying about theory."</h1>
+       <img src={Logo} style={{width:200}}/>
+      <h1 style={{fontSize: 40}}>"Train with both heart and soul without worrying about theory."</h1>
       <p style={{fontSize: 18, margin: 10}}>- Gichin Funakoshi</p>
       <Button variant="contained" color="error" style={{margin: 10, zIndex: 1}}>
         Learn More 
