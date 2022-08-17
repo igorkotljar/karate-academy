5038b2482a920cb4af54c0d8f551a8fa83801147
Igor Kotljar <kotljar.igor@gmail.com>
2022-08-16 21:27:41 -0700

    new header design

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,26 @@
 import React from "react";
 import { Grid } from "@mui/material";
 import bgVideo from "../assets/karate_pirate_video.mp4";
-import bgPhoto from "../assets/kata.jpg"
+import bgPhoto from "../assets/bodybg.webp";
 import Welcome from "./Welcome";
 import SocialLinks from "./SocialLinks";
+import NavBar from "../components/NavBar";
 
 function Header() {
   return (
     <div className="mainHeader">
       <Grid container>
         <Grid xs={12}>
-          <div className="welcome">
+          <div className="backgroundImage">
             <Grid xs={12}>
-            <SocialLinks/>
+              <NavBar />
             </Grid>
-            <video src={bgVideo} autoPlay loop muted />
-            <Grid xs={12} md={5} lg={5}>
-              <Welcome />
+            <Grid xs={12}>
+              <div className="grayContainer"></div>
+              <div className="welcomeMessage">
+                <h4>WE TEACH ALL ASPECTS OF</h4>
+                <h2 style={{fontSize:100}}>KARATE</h2>
+              </div>
             </Grid>
           </div>
         </Grid>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,18 +4,12 @@ import { Grid } from "@mui/material";
 import Header from "../components/Header";
 
 function Home() {
-
-
   return (
     <div>
       <Grid container>
         <Grid xs={12}>
-          <NavBar />
+          <Header />
         </Grid>
-        <Grid xs={12}>
-          <Header/>
-          
-        </Grid>   
       </Grid>
     </div>
   );
